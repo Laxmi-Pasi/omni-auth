9e849810736df3d3aff152ff3d13d22aebdb5ae0
Laxmi-Pasi <laxmip.@simformsolutions.com>
2022-06-20 12:18:41 +0530

    feat: add log out button on home page and profile and put alert and notice

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: 'omniauth'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'users/home', to: "users_controller#home"
+  get 'users/profile'
+  root 'users#home'
 end
